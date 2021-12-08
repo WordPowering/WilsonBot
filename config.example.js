d5e40e28920efb69ad6fc1f6e32cfd27acacf6c0
Dan <70652226+WordPowering@users.noreply.github.com>
2021-12-08 18:36:03 +0100

    Update config.example.js

--- config.example.js
+++ config.example.js
@@ -1,7 +1,7 @@
 const config = {
     // Login info
-    email: 'fuckmedaddy@gmail.com',
-    password: 'robloxisthebesttt2232',
+    email: '12131231231232ew2rw@gmail.com',
+    password: 'eererererererererer',
     // Your id. You can get this via the whoami command
     ownerID: '2d291BZ4',
     // The ids of your bot admins. Be careful who you give this to
